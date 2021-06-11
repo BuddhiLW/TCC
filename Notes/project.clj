e00e6583b1501f8643dafc9c585613e6a9b9aa37
BuddhiLW <pedrogbranquinho@gmail.com>
2021-06-11 00:46:00 -0300

    Update cronogramas e anotações; simulações

---
+++ Notes/project.clj
@@ -0,0 +1,12 @@
+(defproject demos "0.0.1"
+  :description "Examples for TCC presentation"
+  ;; :url "https://github.com/Clojure2D/clojure2d-examples"
+  :license {:name "The Unlicense"
+            :url "http://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 ;; [net.littleredcomputer/sicmutils "0.12.2-SNAPSHOT"]
+                 [sicmutils/sicmutils "0.17.0"]
+                 [clojure2d "1.4.3"]]  
+  :repl-options {:timeout 120000}
+  :target-path "target/%s"
+  :jvm-opts ["-Xmx4096M"])
